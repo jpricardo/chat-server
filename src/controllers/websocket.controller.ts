e63d444e6d8a8a02a8a3c882ea28a9ad267ba841
Ativa Suporte <suporteativanautica@gmail.com>
2022-03-29 09:51:00 -0300

    Sistema de usuários funcionando

--- src/controllers/websocket.controller.ts
+++ src/controllers/websocket.controller.ts
@@ -3,28 +3,40 @@
 	const config = async (server) => {
 		const socketIo = require('socket.io');
 		const io = new socketIo.Server(server);
-		io.on('connection', (socket) => {
-			console.log(`[WEBSOCKET] Usuário se conectou!`);
-			socket.broadcast.emit(
-				'joined',
-				JSON.stringify({
-					user: null,
-					data: 'Usuário se juntou!',
-				})
-			);
 
-			socket.on('disconnect', () => {
-				console.log(`[WEBSOCKET] Usuário desconectado!`);
-				const msg = JSON.stringify({
-					user: null,
-					data: 'Usuário deixou a sala...',
+		let users: { userId: string; name: string }[] = [];
+
+		const findUser = (id: string) => {
+			return users.find((user) => user.userId === id);
+		};
+
+		io.on('connection', (socket) => {
+			socket.on('setId', (data) => {
+				console.log(`[WEBSOCKET] Novo usuário: ${data.name}`);
+				socket.broadcast.emit('joined', {
+					data: `Usuário ${data.name} se juntou!`,
 				});
-				socket.broadcast.emit('left', msg);
+				users.push(data);
 			});
 
 			socket.on('chat message', (msg) => {
-				console.log(`[WEBSOCKET] Nova mensagem => ${msg}`);
-				socket.broadcast.emit('chat message', msg);
+				const user = findUser(socket.id);
+				if (!user) {
+					console.log('[WEBSOCKET] USUÁRIO NÃO ENCONTRADO!');
+					return;
+				}
+				console.log(`[WEBSOCKET] ${user.name} diz: ${msg.data}`);
+				socket.broadcast.emit('chat message', {
+					name: user.name,
+					data: msg.data,
+				});
+			});
+
+			socket.on('disconnect', () => {
+				const user = findUser(socket.id);
+				console.log(`[WEBSOCKET] Usuário ${user.name} desconectado!`);
+				const msg = { data: `${user.name} deixou a sala...` };
+				socket.broadcast.emit('left', msg);
 			});
 		});
 	};
---
+++ src/public/script/chat.ts
@@ -0,0 +1,77 @@
+let username;
+
+const socket = io();
+
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input: HTMLInputElement = document.getElementById('input');
+
+const createEntry = (
+	container: HTMLElement,
+	content: string | { name: string | undefined; data: string },
+	decorator = false,
+	className: string = ''
+) => {
+	if (typeof content === 'string') {
+		content = JSON.parse(content);
+	}
+	const item = document.createElement('li');
+	let text = '';
+
+	if (decorator) {
+		const prefix = `${content.name} [${new Date().toLocaleTimeString()}] >>`;
+		text = `${prefix} ${content.data}`;
+	} else {
+		text = content.data;
+	}
+
+	if (className) {
+		item.classList.add(className);
+	}
+
+	item.textContent = text;
+	container.appendChild(item);
+	window.scrollTo(0, document.body.scrollHeight);
+};
+
+window.onload = () => {
+	while (!username) {
+		username = prompt('Seu nome:');
+	}
+
+	createEntry(
+		messages,
+		{ data: `Bem vind@ à sala, ${username}!` },
+		false,
+		'info'
+	);
+};
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (input.value) {
+		const msg = { name: username, data: input.value };
+		socket.emit('chat message', msg);
+		createEntry(messages, msg, true);
+		input.value = '';
+	}
+});
+
+socket.on('connect', () => {
+	const credentials = { name: username, userId: socket.id };
+	socket.emit('setId', credentials);
+});
+
+socket.on('chat message', (msg) => {
+	const item = document.createElement('li');
+	createEntry(messages, msg, true, 'dark');
+});
+
+socket.on('joined', (msg) => {
+	const item = document.createElement('li');
+	createEntry(messages, msg, false, 'info');
+});
+
+socket.on('left', (msg) => {
+	createEntry(messages, msg, false, 'info');
+});
